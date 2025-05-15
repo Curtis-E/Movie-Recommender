8e2cd412708c8d51aea3d54dfd7365dfe4601ac6
Curtis-E <engc2984@my.uwstout.edu>
2025-05-14 21:45:19 -0500

    Update and rename main.py to MovieRecommender.py

--- MovieRecommender.py
+++ MovieRecommender.py
@@ -1,17 +1,19 @@
 import numpy as np
 import pandas as pd
 
+# Cosine similarity between movies
 def get_cosine(item_data):
     num_movies = item_data.shape[1]
     cosine_matrix = np.zeros((num_movies, num_movies), dtype=np.double)
 
-
+# Loops over all pairs of movies
     for i in range(num_movies):
         for j in range(num_movies):
             movie_pair = item_data.iloc[:,[i,j]]
             movie_pair = movie_pair.dropna()
             movie_i = movie_pair.iloc[:,0]
             movie_j = movie_pair.iloc[:, 1]
+            # Computes similarity bewteen movies
             cosine_matrix[i,j] = np.dot(movie_i, movie_j)/(np.linalg.norm(movie_i)*np.linalg.norm(movie_j))
 
 
@@ -25,14 +27,14 @@ num_movies = item_data.shape[1]
 
 X= df.iloc[:,1:].to_numpy()
 
+# Loops through each user
 for user in range(X.shape[0]):
     has_rating=~np.isnan(X[user])
+    # Loops over each movie
     for movie in range(X.shape[1]):
         if not has_rating[movie]:
             X[user, movie] = np.dot(X[user, has_rating], cm[movie, has_rating])/np.sum(np.abs(cm[movie, has_rating]))
     X[user, has_rating]= -10.0
-
-#make code values 1-2 = -1 and 3-5 = 1
 
 df.iloc[:,1:] = np.round(X, decimals= 3)
 df.to_csv("recommend.csv")
