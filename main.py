c4e01bc00fa3c163a9329763739103ee146bba47
Curtis-E <engc2984@my.uwstout.edu>
2025-05-14 21:40:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,38 @@
-import pandas as pd
 import numpy as np
-from dask.optimization import inline
-from sklearn.linear_model import LinearRegression
-import matplotlib as plt
-import RegressionLib as rl
+import pandas as pd
+
+def get_cosine(item_data):
+    num_movies = item_data.shape[1]
+    cosine_matrix = np.zeros((num_movies, num_movies), dtype=np.double)
 
 
-#want to find if shots taken have any correlation to goals
-df = pd.read_csv("game_teams_stats.csv")
-
-df.info()
-#52609 in my table
-
-df.dropna(inplace=True)
-
-df.info()
-#wanted to get rid of null data in table
-#29554 in my table
-#56% of my table left
-
-df.drop(columns=['won','settled_in','HoA','head_coach','startRinkSide'], inplace=True)
-df.describe()
-
-#print (df.query("game_id == 2015020693"))
-#wanted to make sure 0 was not being drop from the table
-Scored = df.query("shots > 0 and goals >= 0")
-
-X = Scored[["shots", "pim", "takeaways","powerPlayOpportunities"]]
-y = Scored["goals"]
-reg = LinearRegression().fit(X,y)
+    for i in range(num_movies):
+        for j in range(num_movies):
+            movie_pair = item_data.iloc[:,[i,j]]
+            movie_pair = movie_pair.dropna()
+            movie_i = movie_pair.iloc[:,0]
+            movie_j = movie_pair.iloc[:, 1]
+            cosine_matrix[i,j] = np.dot(movie_i, movie_j)/(np.linalg.norm(movie_i)*np.linalg.norm(movie_j))
 
 
-print("skikit learn score")
-print(reg.score(X,y))
+    return cosine_matrix
 
-#X = Scored[["shots",  "takeaways"]]
-#y = Scored["goals"]
+df = pd.read_csv("MovieRecommender.csv")
+item_data = df.iloc[:,1:].copy()
+cm = get_cosine(item_data)
+pd.DataFrame(np.round(cm, decimals=3)).to_csv("similar.csv")
+num_movies = item_data.shape[1]
 
+X= df.iloc[:,1:].to_numpy()
 
+for user in range(X.shape[0]):
+    has_rating=~np.isnan(X[user])
+    for movie in range(X.shape[1]):
+        if not has_rating[movie]:
+            X[user, movie] = np.dot(X[user, has_rating], cm[movie, has_rating])/np.sum(np.abs(cm[movie, has_rating]))
+    X[user, has_rating]= -10.0
 
-print("my Library")
-reg = rl.LinearRegression().fit(X,y)
-print(reg.score(X,y))
+#make code values 1-2 = -1 and 3-5 = 1
 
-
-
-print (Scored[["shots", "goals", "takeaways","powerPlayOpportunities"]].corr())
-
-
-
-
+df.iloc[:,1:] = np.round(X, decimals= 3)
+df.to_csv("recommend.csv")
